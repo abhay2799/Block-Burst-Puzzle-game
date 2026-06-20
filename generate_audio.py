dd39461c06575a9bb5ff12c95e12eb206fdf7719
Abhay Chauhan <abhaychauhan2799@gmail.com>
2026-06-21 03:55:18 +0530

    Update combo voice to energetic male voice

--- generate_audio.py
+++ generate_audio.py
@@ -1,6 +1,11 @@
 import os
 
-words = ["good", "great", "excellent", "amazing", "wonderful", "genius", "master"]
+# We'll use RogerNeural which is listed as "Lively" or GuyNeural "Passion"
+# Let's use GuyNeural with increased rate and pitch for an excited tone.
+voice = "en-US-RogerNeural"
+words = ["wow", "good", "great", "excellent", "amazing", "wonderful", "genius", "master"]
+
 for word in words:
-    cmd = f'python -m edge_tts --voice en-US-AriaNeural --rate=+10% --pitch=+20Hz --text "{word.capitalize()}!" --write-media "public/assets/audio/voice_{word}.mp3"'
+    # +15% rate and +25Hz pitch makes it sound more energetic and excited
+    cmd = f'python -m edge_tts --voice {voice} --rate=+15% --pitch=+25Hz --text "{word.capitalize()}!" --write-media "public/assets/audio/voice_{word}.mp3"'
     os.system(cmd)
